e2a3a8f8fc92d87cc5fb3f9e00b061ac2bc1a5d6
ipshitaRB <ipshitarburman@gmail.com>
2020-02-10 15:14:25 -0500

    Return size of the sample array added to the Adapter

--- app/src/main/java/com/droidrbi/choredom/TodoListAdapter.kt
+++ app/src/main/java/com/droidrbi/choredom/TodoListAdapter.kt
@@ -4,12 +4,15 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
+
+    private val todoList = arrayOf<String> ("Android Development","House Work", "Errands")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return todoList.size
     }
 
     override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
