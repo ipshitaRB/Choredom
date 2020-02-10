d4f4113877bdb9be0fe433e77e17d80c2c91a361
ipshitaRB <ipshitarburman@gmail.com>
2020-02-10 14:36:56 -0500

    Add recyclerview to mainActivity

--- app/src/main/java/com/droidrbi/choredom/MainActivity.kt
+++ app/src/main/java/com/droidrbi/choredom/MainActivity.kt
@@ -5,15 +5,22 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var todoListerRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        todoListerRecyclerView = findViewById(R.id.lists_recyclerview)
+        todoListerRecyclerView.layoutManager = LinearLayoutManager(this)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
