18e74d83d600da7d38425a8f5487d48ff35f1622
ipshitaRB <ipshitarburman@gmail.com>
2020-02-10 16:55:57 -0500

    Implement onBindViewHolder

--- app/src/main/java/com/droidrbi/choredom/TodoListAdapter.kt
+++ app/src/main/java/com/droidrbi/choredom/TodoListAdapter.kt
@@ -19,6 +19,7 @@ class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
-
+        holder.listPosititonTextView.text = (position + 1).toString()
+        holder.listTitleTextView.text = todoList[position]
     }
 }
