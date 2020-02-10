094040cf6187ed5897120f30bce9dc98d23c9dc5
ipshitaRB <ipshitarburman@gmail.com>
2020-02-10 14:57:02 -0500

    Add RecyclerView Adapter implementation

--- app/src/main/java/com/droidrbi/choredom/TodoListAdapter.kt
+++ app/src/main/java/com/droidrbi/choredom/TodoListAdapter.kt
@@ -1,4 +1,18 @@
 package com.droidrbi.choredom
 
-class TodoListAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
