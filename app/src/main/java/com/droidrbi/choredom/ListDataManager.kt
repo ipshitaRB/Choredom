be306e844c0013d7f554c5b3df9669caa564ae0d
ipshitaRB <ipshitarburman@gmail.com>
2020-02-27 14:28:04 -0500

    Create method to retrieve lists from shared preferences

--- app/src/main/java/com/droidrbi/choredom/ListDataManager.kt
+++ app/src/main/java/com/droidrbi/choredom/ListDataManager.kt
@@ -14,4 +14,16 @@ class ListDataManager(private val context: Context) {
         sharedPrefs.apply()
     }
 
+    fun readLists(): ArrayList<TaskList>{
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val contents = sharedPrefs.all
+        val taskLists = ArrayList<TaskList>()
+
+        for (taskList in contents){
+            val taskItems = ArrayList(taskList.value as HashSet<String>)
+            val list = TaskList(taskList.key, taskItems)
+            taskLists.add(list)
+        }
+        return taskLists
+    }
 }
